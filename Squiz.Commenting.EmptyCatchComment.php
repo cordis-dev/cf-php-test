ecbc7ebdf02f7bee60681f5d14ffc04dcde31acb
cordis-dev <cordis.dev@gmail.com>
2017-11-08 11:08:28 +0200

    Create Squiz.Commenting.EmptyCatchComment.php

---
+++ Squiz.Commenting.EmptyCatchComment.php
@@ -0,0 +1,31 @@
+<?php
+
+class SomeClass
+{
+  public function saveFile($filename)
+  {
+    $file = PhpFile::instance($filename);
+
+    try {
+      $file->lock();
+    } catch (Exception $ex) {
+
+    }
+  }
+}
+
+class SomeClass2
+{
+  public function saveFile($filename)
+  {
+    $file = PhpFile::instance($filename);
+
+    try {
+      $file->lock();
+    } catch (Exception $ex) {
+      // handle
+    }
+  }
+}
+
+?>
